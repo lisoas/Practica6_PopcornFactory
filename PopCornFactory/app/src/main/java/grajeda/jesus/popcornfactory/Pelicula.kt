8bb152cb2675d79c5d0cae182b682d3008217a5a
Jesus Grajeda <lisoas8@gmail.com>
2021-03-07 22:51:29 -0700

    Practica 6

---
+++ PopCornFactory/app/src/main/java/grajeda/jesus/popcornfactory/Pelicula.kt
@@ -0,0 +1,7 @@
+package grajeda.jesus.popcornfactory
+
+data class Pelicula (var titulo: String,
+                     var image: Int,
+                     var header: Int,
+                     var sinopsis: String){
+}
